291f42b4399e50e956b19789f12b963718e3963b
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-16 16:11:46 +0200

    Ara ja podem modificar el títol

--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/EditTitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/EditTitleActivity.java
@@ -1,13 +1,34 @@
 package edu.upc.eseiaat.pma.titleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EditTitleActivity extends AppCompatActivity {
+
+    private EditText edit_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_title);
+
+        //(II)
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        edit_title = (EditText) findViewById(R.id.edit_title);
+        edit_title.setText(title);
+    }
+
+    public void saveTitle(View view) {
+        //(III)
+        String new_title = edit_title.getText().toString();
+        //new intent buit i li afegim el title nou, modificat
+        Intent data = new Intent();
+        data.putExtra("title", new_title);
+        setResult(RESULT_OK, data);
+        finish();
     }
 }
--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
@@ -4,19 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class TitleActivity extends AppCompatActivity {
+
+    private String title;
+    private TextView title_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        title = getResources().getString(R.string.title);
         setContentView(R.layout.activity_title);
+        title_text = (TextView) findViewById(R.id.title);
+        title_text.setText(title);
     }
 
     public void editTitle(View view) {
+        //(I)
         //1. Crear un intent
         Intent intent = new Intent(this,EditTitleActivity.class);
-        //2. Passar l'intent a Android perquè inicialitzi l'activitat
-        startActivity(intent);
+        //2. Afegir paràmetre (dades extra) a la crida de l'activitat
+        intent.putExtra("title", title);
+        //3. Passar l'intent a Android perquè inicialitzi l'activitat
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 0:
+                // (IV)
+                if (resultCode == AppCompatActivity.RESULT_OK) {
+                    String new_title = data.getStringExtra("title");
+                    title = new_title;
+                    title_text.setText(title);
+                }
+        }
     }
 }
